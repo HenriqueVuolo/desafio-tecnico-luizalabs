f6dd14841f0beef25467bc2e171ff6ef489658e5
HenriqueVuolo <henriquevuolo@gmail.com>
2023-12-03 15:09:30 -0300

    feat: add and config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder().setTitle('API').setVersion('1.0').build(),
+    ),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
