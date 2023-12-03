48009c843f58c890881a86358e29e070a516929e
HenriqueVuolo <henriquevuolo@gmail.com>
2023-12-03 18:47:04 -0300

    feat: add PrismaUserMapper

---
+++ src/infra/database/prisma/mappers/prisma-user.mappers.ts
@@ -0,0 +1,26 @@
+import {User as PrismaUser, Order as PrismaOrder} from '@prisma/client';
+import {User} from '@domain/entities/user.entity';
+import {PrismaOrderMapper} from './prisma-order.mappers';
+
+export class PrismaUserMapper {
+  static toPrisma(user: User): PrismaUser {
+    const {user_id, name} = new User(user);
+    return {
+      id: user_id,
+      name,
+    };
+  }
+  static toDomain(user: PrismaUser): User {
+    return new User({
+      user_id: user.id,
+      name: user.name,
+    });
+  }
+  static withOrderToDomain(user: PrismaUser & {orders: PrismaOrder[]}): User {
+    return new User({
+      user_id: user.id,
+      name: user.name,
+      orders: user.orders.map((order) => PrismaOrderMapper.toDomain(order)),
+    });
+  }
+}
