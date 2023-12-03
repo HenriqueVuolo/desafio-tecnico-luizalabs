5a1428f3a5f2f3e1d0cb7a5707e94c44ce1c247a
HenriqueVuolo <henriquevuolo@gmail.com>
2023-12-03 20:10:59 -0300

    feat: add prisma repository implementations to DatabaseModule

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,22 @@
 import {Module} from '@nestjs/common';
 import {PrismaService} from './prisma/prisma.service';
+import {UserRepository} from '@domain/repositories/user.repository';
+import {PrismaUserRepository} from './prisma/repositories/prisma-user.repository';
+import {OrderRepository} from '@domain/repositories/order.repository';
+import {PrismaOrderRepository} from './prisma/repositories/prisma-order.repository';
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository,
+    },
+    {
+      provide: OrderRepository,
+      useClass: PrismaOrderRepository,
+    },
+  ],
+  exports: [UserRepository, OrderRepository],
 })
 export class DatabaseModule {}
